89a7c9853b8225009b186092249114fe6982b1fa
vbarron <vbb@wayakna.com>
2024-06-24 10:56:10 -0600

    add grocery tracker file

--- CalculateArea/calculate_area.js
+++ CalculateArea/calculate_area.js
@@ -8,3 +8,11 @@ function calculateArea() {
     let area = length*width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+
+function calculateTotal() {
+    const g1 = parseFloat(document.getElementById('grocery1').value);
+    const g2 = parseFloat(document.getElementById('grocery2').value);
+    const g3 = parseFloat(document.getElementById('grocery3').value);
+
+    document.getElementById('result').innerText = `The total of groceries is: $ ${Math.round(g1+g2+g3)}`;
+}
